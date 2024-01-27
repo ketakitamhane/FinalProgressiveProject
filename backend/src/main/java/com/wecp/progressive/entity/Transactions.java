5be44a38c8cc04469cabaa532a20ebb6be744233
username <wecp-dummy@gmail.com>
2024-01-27 11:21:08 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/entity/Transactions.java
+++ backend/src/main/java/com/wecp/progressive/entity/Transactions.java
@@ -3,6 +3,55 @@ import java.util.Date;
 
 
 public class Transactions {
+    private int transaction_id;
+    private double amount;
+    private Date transaction_date;
+    private String transaction_type;
+
+    public Transactions(){}
+
+    public Transactions(int transaction_id, double amount, Date transaction_date, String transaction_type) {
+        this.transaction_id = transaction_id;
+        this.amount = amount;
+        this.transaction_date = transaction_date;
+        this.transaction_type = transaction_type;
+    }
+
+    public int getTransaction_id() {
+        return transaction_id;
+    }
+
+    public void setTransaction_id(int transaction_id) {
+        this.transaction_id = transaction_id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getTransaction_date() {
+        return transaction_date;
+    }
+
+    public void setTransaction_date(Date transaction_date) {
+        this.transaction_date = transaction_date;
+    }
+
+    public String getTransaction_type() {
+        return transaction_type;
+    }
+
+    public void setTransaction_type(String transaction_type) {
+        this.transaction_type = transaction_type;
+    }
+
+    
+
+    
 
 
 }
